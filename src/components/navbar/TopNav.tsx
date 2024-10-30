3e577687b661601049b13f2e3492ebae1b5d007b
supermanorta <supermanorta@yahoo.com>
2024-10-30 14:20:37 -0500

    Added some styling to the NavBar and outsource some styling to the gobal file

--- src/components/navbar/TopNav.tsx
+++ src/components/navbar/TopNav.tsx
@@ -1,23 +1,37 @@
-import { Navbar, NavbarBrand, NavbarContent } from '@nextui-org/react'
+import { Button, Link, Navbar, NavbarBrand, NavbarContent, NavbarItem } from '@nextui-org/react'
 import React from 'react'
 import { GiMatchTip } from 'react-icons/gi'
 
 export default function TopNav(  ) {
   return (
     <div>
-      <Navbar>
+      <Navbar 
+      className='bg-gradient-to-r from-purple-400 to-purple-700' 
+      maxWidth='xl'
+      classNames={{
+        item:[
+            'text-xl',
+            'text-white',
+            'uppercase'
+        ]
+      }}>
         <NavbarBrand>
             <GiMatchTip size={40}/>
-            <div>
+            <div className='font-bold text-3xl flex'>
                 <span>Next</span>
                 <span>Match</span>
             </div>
         </NavbarBrand>
         <NavbarContent justify='center'>
-            Center
+            <NavbarItem as={Link} href='/members'>Matches</NavbarItem>
+            <NavbarItem as={Link} href='/lists'>Matches</NavbarItem>
+            <NavbarItem as={Link} href='/messages'>Matches</NavbarItem>
         </NavbarContent>
         <NavbarContent justify='end'>
-            Right(end)
+           
+           <Button className='user-initial-act' variant="bordered" > Login </Button>
+           <Button className='user-initial-act' variant='bordered'> Register </Button>
+
         </NavbarContent>
       </Navbar>
     </div>
