3300dcbe377ca858720baa08e3d4c4c3eb65b060
supermanorta <supermanorta@yahoo.com>
2024-10-31 16:45:17 -0500

    Added pages needed from the NavBar with active links to the pages.

--- src/components/navbar/TopNav.tsx
+++ src/components/navbar/TopNav.tsx
@@ -15,22 +15,23 @@ export default function TopNav(  ) {
             'uppercase'
         ]
       }}>
-        <NavbarBrand>
-            <GiMatchTip size={40}/>
-            <div className='font-bold text-3xl flex'>
-                <span>Next</span>
-                <span>Match</span>
+        <NavbarBrand as={Link} href='/'>
+            <GiMatchTip className='text-gray-200' size={40}/>
+            <div className='font-bold text-3xl  flex'>
+                  <span className='text-gray-900'>Next</span>
+                  <span className='text-gray-200'>Match</span>             
             </div>
         </NavbarBrand>
         <NavbarContent justify='center'>
-            <NavbarItem as={Link} href='/members'>Matches</NavbarItem>
-            <NavbarItem as={Link} href='/lists'>Matches</NavbarItem>
-            <NavbarItem as={Link} href='/messages'>Matches</NavbarItem>
+            <NavbarItem as={Link} href='/members'>Members</NavbarItem>
+            <NavbarItem as={Link} href='/lists'>Lists</NavbarItem>
+            <NavbarItem as={Link} href='/messages'>Messages</NavbarItem>
+            {/* <NavbarItem as={Link} href='/'>Matches</NavbarItem> */}
         </NavbarContent>
         <NavbarContent justify='end'>
            
-           <Button className='user-initial-act' variant="bordered" > Login </Button>
-           <Button className='user-initial-act' variant='bordered'> Register </Button>
+           <Button className='user-initial-act' variant="bordered"  as={Link} href='/login'> Login </Button>
+           <Button className='user-initial-act' variant='bordered' as={Link} href='/register'> Register </Button>
 
         </NavbarContent>
       </Navbar>
